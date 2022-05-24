e76af296dbd3a44b1b1e5d1fa39d062d61d5b27e
Gilded <knukles30@gmail.com>
2022-05-24 16:12:13 -0600

    More Lectures Done, Hope i can do the Basic Operators ASAP

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,20 @@
-let country = "Mexico";
-let continent = "America";
+//LECTURE: Values and Variables
+const country = "Mexico";
+const continent = "America";
 let population = "128.9 Millons";
 console.log(country);
 console.log(continent);
 console.log(population);
-/*asdasd*/
+
+//LECTURE: Data Types
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+//LECTURE: let const and var
+language = "Spanish";
+
+//LECTURE: Basic Operators
