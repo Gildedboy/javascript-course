7a629a02526816ddd20fc5307254b3fcc9a0b0b3
Gilded <knukles30@gmail.com>
2022-06-11 09:31:00 -0600

    added switch statement lecture and challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -87,3 +87,43 @@ if (language === "english" && population < 50 && !isIsland) {
 } else {
   console.log(`${country} doesn't meet your criteria :(`);
 }
+
+//LECTURE: The Switch Statement
+// const day = `monday`;
+
+// //using if-else instead of SWITCH CASE
+// if (day === `monday`) {
+//   console.log(`Plan Course Structure`);
+//   console.log(`Go to coding meetup`);
+// } else if (day === `tuesday`) {
+//   console.log(`Prepare theory videos`);
+// } else if (day === `wednesday` || day === `thursday`) {
+//   console.log(`Write code examples`);
+// } else if (day === `friday`) {
+//   console.log(`Record videos`);
+// } else if (day === `saturday` || day === `sunday`) {
+//   console.log(`Enjoy the weekend :D`);
+// } else {
+//   console.log("Not a valid day!");
+// }
+switch (language) {
+  case `chinese`:
+  case `mandarin`:
+    console.log(`MOST number of native speakers`);
+    break;
+  case `spanish`:
+    console.log(`2nd place in number of native speakers`);
+    break;
+  case `english`:
+    console.log(`3rd place`);
+    break;
+  case `hindi`:
+    console.log(`Number 4`);
+    break;
+  case `arabic`:
+    console.log(`5th most spoken language`);
+    break;
+  default:
+    console.log(`Great language too :D`);
+    break;
+}
