910e2bbd120c93162839a2b45f5fd5cdfadf9b09
Gilded <knukles30@gmail.com>
2022-06-20 14:20:38 -0600

    Function assignment complete

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,1 +1,11 @@
 "use strict";
+//FUNCTIONS
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+const mexico = describeCountry("Mexico", 128.9, "CDMX");
+const canada = describeCountry("Canada", 30.8, "Ottawa");
+const japon = describeCountry("Japon", 125.8, "Tokyo");
+console.log(mexico);
+console.log(canada);
+console.log(japon);
